3186115d1a4cebea7fb80c0011b1e34d8cb3b1c6
sara-sonia <saramartinsen@outlook.com>
2025-11-23 23:22:20 +0100

    Setup Google Maps API key and configuration

--- script.js
+++ script.js
@@ -13,3 +13,32 @@ document.addEventListener('DOMContentLoaded', () => {
     popoverMenu.classList.remove('active');
   });
 });
+
+
+function initMap() {
+  const map = new google.maps.Map(document.getElementById("googleMap"), {
+    zoom: 15, 
+    center: { lat: 0, lng: 0 }, 
+  });
+
+  const geocoder = new google.maps.Geocoder();
+  const address = "Oppdalslinna 242, 2740 Roa";
+
+  geocoder.geocode({ address: address }, (results, status) => {
+    if (status === "OK" && results[0]) {
+      const location = results[0].geometry.location;
+
+      
+      map.setCenter(location);
+
+      
+      new google.maps.Marker({
+        map: map,
+        position: location,
+        title: "Roa Address",
+      });
+    } else {
+      alert("Geocode was not successful for the following reason: " + status);
+    }
+  });
+}
